0d21748b05d12c0ad4dfc73627c3fb415f4255fc
Bethany Whitworth <bethanyjwhitworth@gmail.com>
2020-07-28 18:45:01 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Howdy Y'all!!");
+            Console.ReadKey(); // What happens if you run the app without this line?
+
+            var animals = new string[] { "Triceratops", "Gorilla", "Corgi", "Toucan", "Cat" };
+
+            var vowels = new char[] {a, e, i, o, u, y}
+
+            foreach (var animal in animals)
+            {
+          
+                Console.WriteLine(animal);
+            }
+
+        }
+    }
+}
